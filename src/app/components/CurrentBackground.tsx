ead96252793dad6a55b3ac7761ced003c7976576
GustavoSouza <juvinogustavo1@gmail.com>
2023-08-18 12:14:49 -0300

    bug: fix bug with main images

--- src/app/components/CurrentBackground.tsx
+++ src/app/components/CurrentBackground.tsx
@@ -5,7 +5,6 @@ import { useGlobalContext } from '../context/store'
 import Image from 'next/image'
 import daytime from 'public/assets/desktop/bg-image-daytime.jpg'
 import nightTime from 'public/assets/desktop/bg-image-nighttime.jpg'
-import { Skeleton } from '@mui/material'
 
 const CurrentBackground = () => {
   const [backgroundURL, setBackgroundURL] = useState<string>()
@@ -23,28 +22,17 @@ const CurrentBackground = () => {
     }
   }, [message])
 
-  if (!backgroundURL)
-    return (
-      <Skeleton
-        sx={{ bgcolor: 'grey.900' }}
-        width={'100%'}
-        height={'100%'}
-        variant="rectangular"
-        className="absolute z-50"
-      />
-    )
-  else
-    return (
-      <Image
-        width={1440}
-        height={800}
-        alt="background-image"
-        src={backgroundURL === 'nighttime' ? nightTime : daytime}
-        className="absolute z-30 h-full w-full object-cover"
-        priority={true}
-        placeholder="blur"
-      />
-    )
+  return (
+    <Image
+      width={1440}
+      height={800}
+      alt="background-image"
+      src={backgroundURL === 'nighttime' ? nightTime : daytime}
+      className="absolute z-30 h-full w-full object-cover"
+      priority={true}
+      placeholder="blur"
+    />
+  )
 }
 
 export default CurrentBackground
